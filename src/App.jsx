7e306024bd0ef9d9e1d27ab7182ce4ba79a0e5e2
Dawid Urbaniak <udawid00@gmail.com>
2023-11-09 10:42:08 +0100

    Outputting Dynamic Content

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,54 @@
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[getRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
 }
+
+function CoreConcepts({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
