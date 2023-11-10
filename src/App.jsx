a13d53a9129fabe7214808fa2c0f3753fbde8e17
Dawid Urbaniak <udawid00@gmail.com>
2023-11-10 10:52:55 +0100

    Passing Custom Arguments to Event Functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { CORE_CONCEPTS } from "./data";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 
 function App() {
+  function handleSelect(selectedButton) {
+    // selectedButton =>'components','jsx', 'props', 'state'
+    console.log(selectedButton);
+  }
   return (
     <div>
       <Header />
@@ -19,6 +24,18 @@ function App() {
             <CoreConcepts {...CORE_CONCEPTS[2]} />
             <CoreConcepts {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
